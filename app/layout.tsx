85e5ff4acf4ae3e77f65788fbd4a4621cc9e3324
John Rey Lumangyao <lumangyao112@gmail.com>
2023-09-14 21:23:30 +0800

    add maps and sign up page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,8 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import { Navigation } from './resources/components'
+import './globals.css';
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import { Navigation } from './resources/components';
+
 
 const inter = Inter({ subsets: ['latin'] })
 
--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,5 +1,31 @@
+'use client';;
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+import L, { LatLngExpression } from 'leaflet';
+
 export default function SignUp() {
+  const icon = require('../../public/Images/marker.png');
+  const leafletIcon = new L.DivIcon({
+   className:' bg-red-500 rounded-full h-[20px] w-[20px]',
+    iconSize:[30,30]
+  });
+  const position:LatLngExpression = [10.3157, 123.8854];
   return (
-    <div className=" pt-32">SignUp</div>
+    <div className=" pt-32">
+    
+       <MapContainer center={position} zoom={13} className=' z-20'>
+      
+      
+    <TileLayer
+    className=' w-full'
+      attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+    />
+    <Marker position={[10.3157, 123.8854]} icon={leafletIcon}>
+      <Popup>
+        A pretty CSS3 popup. <br /> Easily customizable.
+      </Popup>
+    </Marker>
+  </MapContainer></div>
   )
 }
